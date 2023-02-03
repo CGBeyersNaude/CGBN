19a1a2b42610dcdd46a09d5b472d487124c807cd
tj1603 <cgbeyersnaude1234567890@gmail.com>
2023-02-03 16:28:34 +0100

    asdfghjsxdcfvgbh

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ window.onload = function() {
 
 const hamburger_button = document.querySelector('.hamburger');
 const hamburger_menu = document.querySelector('.hamburger-menu-container');
+const hamburger_container_container = document.querySelector('.hamburger-menu-container-container');
 const button_icon_hamburger = '<span class="material-symbols-outlined">menu</span>';
 const button_icon_cross = '<span class="material-symbols-outlined">close</span>';
 
 hamburger_button.addEventListener('click', function() {
     hamburger_button.classList.toggle('is-active');
     hamburger_menu.classList.toggle('is-active');
+    hamburger_container_container.classList.toggle('is-active');
 
     if (hamburger_button.innerHTML == button_icon_hamburger) {
         hamburger_button.innerHTML = button_icon_cross;
