0b68448f86b460cfee2e709095acb3d11efaf087
tj1603 <cgbeyersnaude1234567890@gmail.com>
2023-02-03 15:59:22 +0100

    dark theme

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 window.onload = function() {
-
+    ChangeTheme();
 };
 
 
 const hamburger_button = document.querySelector('.hamburger');
 const hamburger_menu = document.querySelector('.hamburger-menu-container');
-
 const button_icon_hamburger = '<span class="material-symbols-outlined">menu</span>';
 const button_icon_cross = '<span class="material-symbols-outlined">close</span>';
 
@@ -23,21 +22,31 @@ hamburger_button.addEventListener('click', function() {
 });
 
 
-const storedTheme = localStorage.getItem("theme");
-
-function ChangeTheme() {
-
-}
-
+const storedTheme = localStorage.getItem('theme');
 const theme_button = document.querySelector('.theme-button');
-
 const button_icon_light = '<span class="material-symbols-outlined">light_mode</span>'
 const button_icon_dark = '<span class="material-symbols-outlined">dark_mode</span>'
 
 theme_button.addEventListener('click', function() {
+    let theme = 'light';
     if (theme_button.innerHTML == button_icon_dark) {
         theme_button.innerHTML = button_icon_light;
+        theme = 'light';
     } else {
         theme_button.innerHTML = button_icon_dark;
+        theme = 'dark';
     }
+
+    localStorage.setItem('theme', theme);
+    ChangeTheme();
 });
+
+function ChangeTheme() {
+    if (localStorage.getItem('theme') == 'dark') {
+        document.body.classList.add('dark-mode');
+        theme_button.innerHTML = button_icon_dark;
+    } else {
+        document.body.classList.remove('dark-mode');
+        theme_button.innerHTML = button_icon_light;
+    }
+}
